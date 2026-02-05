1ee5651dc0d2bbfa2042623ad40770f5c9b2abac
apaachee2015-maker <apaachee2015@gmail.com>
2026-02-06 00:30:43 +0500

    last-0602

--- public/index.php
+++ public/index.php
@@ -1,33 +1,35 @@
 <?php
 
-define("ROOT", dirname(__DIR__));
-define("PUBLIC", ROOT . '/public');
-define("CORE", ROOT . '/core');
-define("APP", ROOT . '/app');
-define("CONTROLLERS", APP . '/controllers');
-define("VIEWS", APP . '/views');
-define("PATH", 'http://phplesson.loc');
+
+
+
+require dirname(__DIR__) . '/config/config.php';
 
 require CORE . '/funcs.php';
 
-$uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'], '/');
-//dump($_GET);
-//dump($_SERVER['QUERY_STRING']);
-if ($uri === '') {
-require CONTROLLERS . '/index.php';
+require CORE . '/router.php';
 
-}
-elseif ($uri == 'about') {
-    require CONTROLLERS . '/about.php';
-}
-elseif ($uri == 'post') {
-    dd("SHOW POST");
-}
-else {
 
-    abort();
 
-}
+
+//$uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'], '/');
+////dump($_GET);
+////dump($_SERVER['QUERY_STRING']);
+//if ($uri === '') {
+//    require CONTROLLERS . '/index.php';
+//
+//}
+//elseif ($uri == 'about') {
+//    require CONTROLLERS . '/about.php';
+//}
+//elseif ($uri == 'post') {
+//    dd("SHOW POST");
+//}
+//else {
+//
+//    abort();
+//
+//}
 
 
 
