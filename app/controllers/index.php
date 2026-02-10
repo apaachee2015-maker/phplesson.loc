362f1fe6aae5622d699a490b70c0e8d6dcecfa2c
apaachee2015-maker <apaachee2015@gmail.com>
2026-02-10 23:36:22 +0500

    last-home-10-02

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -3,61 +3,12 @@
 
 
 $title = 'My Blog:: Home';
-$posts = [
-
-        1 => [
-                'title' => 'title1',
-                'desc' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                'slug' => 'title-1'
-        ],
-        2 => [
-                'title' => 'title2',
-                'desc' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                'slug' => 'title-2'
-        ],
-        3 => [
-                'title' => 'title3',
-                'desc' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                'slug' => 'title-3'
-        ],
-        4 => [
-                'title' => 'title4',
-                'desc' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                'slug' => 'title-3'
-        ],
-        5 => [
-                'title' => 'title5',
-                'desc' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                'slug' => 'title-3'
-        ],
-
-
-];
+$posts = $db->query("SELECT * FROM posts ORDER BY id DESC ")->fetchAll();
+$recent_posts = $db->query("SELECT * FROM posts ORDER BY id DESC LIMIT 3")->fetchAll();
 
 //dd($posts);
 
-$recent_posts = [
-        1 => [
-                'title' => 'An item',
-                'slug' => lcfirst(str_replace(' ', '-', 'An item')),
-        ],
-        2 => [
-                'title' => 'A second item',
-                'slug' => lcfirst(str_replace(' ', '-', 'A second item')),
-        ],
-        3 => [
-                'title' => 'A third item',
-                'slug' => lcfirst(str_replace(' ', '-', 'A third item')),
-        ],
-        4 => [
-                'title' => 'A fourth item',
-                'slug' => lcfirst(str_replace(' ', '-', 'A fourth item')),
-        ],
-        5 => [
-                'title' => 'And a fifth one',
-                'slug' => lcfirst(str_replace(' ', '-', 'And a fifth one')),
-        ],
-];
+
 
 require_once VIEWS . '/index.tpl.php';
 
