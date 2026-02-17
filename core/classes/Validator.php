3f72a9f39972fa9622d582094c42514c5c3b5ce8
apaachee2015-maker <apaachee2015@gmail.com>
2026-02-18 01:34:18 +0500

    2 less11-home-18-02

--- core/classes/Validator.php
+++ core/classes/Validator.php
@@ -7,12 +7,30 @@ class Validator {
 
     public function validate($data = [], $rules = [])
     {
-//            print_arr($data);
-//            print_arr($rules);
+
         foreach ($data as $fieldname => $value) {
 
+            if (in_array($fieldname, array_keys($rules)))
+            {
+                $this->check([
+                    'fieldname' => $fieldname,
+                    'value' => $value,
+                    'rules' => $rules[$fieldname],
+
+                ]);
+            }
         }
     }
 
+    protected function check($field)
+    {
+        print_arr($field);
+    }
+
+
+    protected function required($value, $rule_value)
+    {
+        return empty(trim($value));
+    }
 }
 
